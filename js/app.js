304b5a4aa888c97abf2b0339c14dc880ab262188
jbrockdev <jbrockwork33@osgsmail.icu>
2021-08-16 14:35:42 -0700

    Added getDailySales

--- js/app.js
+++ js/app.js
@@ -12,40 +12,145 @@ let restarauntsArray = [
     location: "Seattle",
     minCustomers: 23,
     maxCustomers: 65,
-    avgCookiesPerSale: 6.3
+    avgCookiesPerSale: 6.3,
+    setLocationValue: function(location) {
+      this.location = location;
+    },
+    setMinCustomersValue: function(minCustomers) {
+      this.minCustomers = minCustomers;
+    },
+    setMaxCustomersValue: function(maxCustomers) {
+      this.maxCustomers = maxCustomers;
+    },
+    setAvgCookiesPerSaleValue: function(avgCookiesPerSale) {
+      this.avgCookiesPerSale = avgCookiesPerSale;
+    },
+    getRandomCustomersForHour: function() {
+      let min, max;
+      min = Math.ceil(this.minCustomers);
+      max = Math.floor(this.maxCustomers);
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    },
+    hourlySalesForTheDay: []
   },
   {
     id: 2,
     location: "Tokyo",
     minCustomers: 3,
     maxCustomers: 24,
-    avgCookiesPerSale: 1.2
+    avgCookiesPerSale: 1.2,
+    setLocationValue: function(location) {
+      this.location = location;
+    },
+    setMinCustomersValue: function(minCustomers) {
+      this.minCustomers = minCustomers;
+    },
+    setMaxCustomersValue: function(maxCustomers) {
+      this.maxCustomers = maxCustomers;
+    },
+    setAvgCookiesPerSaleValue: function(avgCookiesPerSale) {
+      this.avgCookiesPerSale = avgCookiesPerSale;
+    },
+    getRandomCustomersForHour: function() {
+      let min, max;
+      min = Math.ceil(this.minCustomers);
+      max = Math.floor(this.maxCustomers);
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    },
+    hourlySalesForTheDay: []
   },
   {
     id: 3,
     location: "Dubai",
     minCustomers: 11,
     maxCustomers: 38,
-    avgCookiesPerSale: 3.7
+    avgCookiesPerSale: 3.7,
+    setLocationValue: function(location) {
+      this.location = location;
+    },
+    setMinCustomersValue: function(minCustomers) {
+      this.minCustomers = minCustomers;
+    },
+    setMaxCustomersValue: function(maxCustomers) {
+      this.maxCustomers = maxCustomers;
+    },
+    setAvgCookiesPerSaleValue: function(avgCookiesPerSale) {
+      this.avgCookiesPerSale = avgCookiesPerSale;
+    },
+    getRandomCustomersForHour: function() {
+      let min, max;
+      min = Math.ceil(this.minCustomers);
+      max = Math.floor(this.maxCustomers);
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    },
+    hourlySalesForTheDay: []
   },
   {
     id: 4,
     location: "Paris",
     minCustomers: 20,
     maxCustomers: 38,
-    avgCookiesPerSale: 2.3
+    avgCookiesPerSale: 2.3,
+    setLocationValue: function(location) {
+      this.location = location;
+    },
+    setMinCustomersValue: function(minCustomers) {
+      this.minCustomers = minCustomers;
+    },
+    setMaxCustomersValue: function(maxCustomers) {
+      this.maxCustomers = maxCustomers;
+    },
+    setAvgCookiesPerSaleValue: function(avgCookiesPerSale) {
+      this.avgCookiesPerSale = avgCookiesPerSale;
+    },
+    getRandomCustomersForHour: function() {
+      let min, max;
+      min = Math.ceil(this.minCustomers);
+      max = Math.floor(this.maxCustomers);
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    },
+    hourlySalesForTheDay: []
   },
   {
     id: 5,
     location: "Lima",
     minCustomers: 2,
     maxCustomers: 16,
-    avgCookiesPerSale: 4.6
+    avgCookiesPerSale: 4.6,
+    setLocationValue: function(location) {
+      this.location = location;
+    },
+    setMinCustomersValue: function(minCustomers) {
+      this.minCustomers = minCustomers;
+    },
+    setMaxCustomersValue: function(maxCustomers) {
+      this.maxCustomers = maxCustomers;
+    },
+    setAvgCookiesPerSaleValue: function(avgCookiesPerSale) {
+      this.avgCookiesPerSale = avgCookiesPerSale;
+    },
+    getRandomCustomersForHour: function() {
+      let min, max;
+      min = Math.ceil(this.minCustomers);
+      max = Math.floor(this.maxCustomers);
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    },
+    hourlySalesForTheDay: []
   }
 ];
 
 restaraunts.addRestaraunt = function(restaraunt) {
-  if (restaraunt.location && restaraunt.minCustomers && restaraunt.maxCustomers && restaraunt.avgCookiesPerSale) {
+  if (restaraunt.location && restaraunt.minCustomers && restaraunt.maxCustomers && restaraunt.avgCookiesPerSale && hourlySalesForTheDay) {
+    this.setLocationValue = function(location) { this.location = location; };
+    this.setMinCustomersValue = function(minCustomers) { this.minCustomers = minCustomers; };
+    this.setMaxCustomersValue = function(maxCustomers) { this.maxCustomers = maxCustomers; };
+    this.setAvgCookiesPerSaleValue = function(avgCookiesPerSale) { this.avgCookiesPerSale = avgCookiesPerSale; };
+    this.getRandomCustomersForHour = function() {
+      let min, max;
+      min = Math.ceil(this.minCustomers);
+      max = Math.floor(this.maxCustomers);
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    };
     this.restarauntsArray.push(restaraunt);
     console.log("Added restaraunt: " + restaraunt.location);
   } else {
@@ -58,5 +163,17 @@ restaraunts.removeRestaraunt = function(id) {
   for (let i = 0; i < restarauntsArray.length; i++) {
     restarauntsArray.splice(i, 1);
     console.log("Removed restaraunt: Store#" + id);
+  }
+}
+
+getDailySales = function() {
+  let cookiesSold = 0;
+  let restaraunt;
+  for (let i = 0; i < restarauntsArray.length; i++) {
+    restaraunt = restarauntsArray[i];
+    for (let j = 0; j < 14; j++) {
+      cookiesSold = restaraunt.getRandomCustomersForHour * restaraunt.avgCookiesPerSale;
+      restaraunt.cookiesSold.push(cookiesSold);
+    }
   }
 }
