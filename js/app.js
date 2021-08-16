dd9ea8f17b2db788d91b56a4a96a9e0245204161
jbrockdev <jbrockwork33@osgsmail.icu>
2021-08-16 11:34:59 -0700

    Added functionality for the add/remove of restaraunts.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 console.log("Initialized!");
 
-let restaraunt;
 const restaraunts = {
   restarauntsArray: [],
   addRestaraunt: null,
@@ -9,30 +8,35 @@ const restaraunts = {
 
 let restarauntsArray = [
   {
+    id: 1,
     location: "Seattle",
     minCustomers: 23,
     maxCustomers: 65,
     avgCookiesPerSale: 6.3
   },
   {
+    id: 2,
     location: "Tokyo",
     minCustomers: 3,
     maxCustomers: 24,
     avgCookiesPerSale: 1.2
   },
   {
+    id: 3,
     location: "Dubai",
     minCustomers: 11,
     maxCustomers: 38,
     avgCookiesPerSale: 3.7
   },
   {
+    id: 4,
     location: "Paris",
     minCustomers: 20,
     maxCustomers: 38,
     avgCookiesPerSale: 2.3
   },
   {
+    id: 5,
     location: "Lima",
     minCustomers: 2,
     maxCustomers: 16,
@@ -40,10 +44,19 @@ let restarauntsArray = [
   }
 ];
 
-restaraunts.addRestaraunt = function() {
-
+restaraunts.addRestaraunt = function(restaraunt) {
+  if (restaraunt.location && restaraunt.minCustomers && restaraunt.maxCustomers && restaraunt.avgCookiesPerSale) {
+    this.restarauntsArray.push(restaraunt);
+    console.log("Added restaraunt: " + restaraunt.location);
+  } else {
+    console.log("Error adding restaraunt.");
+  }
 }
 
-restaraunts.removeRestaraunt = function() {
-
+restaraunts.removeRestaraunt = function(id) {
+  restarauntsArray = this.restarauntsArray;
+  for (let i = 0; i < restarauntsArray.length; i++) {
+    restarauntsArray.splice(i, 1);
+    console.log("Removed restaraunt: Store#" + id);
+  }
 }
