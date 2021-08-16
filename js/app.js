f72e236ef9825732fa2ff890b5f13769836ed448
jbrockdev <jbrockwork33@osgsmail.icu>
2021-08-16 10:50:50 -0700

    Created Object for restaraunt fronts containing an array of fronts and add/remove function.

---
+++ js/app.js
@@ -0,0 +1,49 @@
+console.log("Initialized!");
+
+let restaraunt;
+const restaraunts = {
+  restarauntsArray: [],
+  addRestaraunt: null,
+  removeRestaraunt: null
+};
+
+let restarauntsArray = [
+  {
+    location: "Seattle",
+    minCustomers: 23,
+    maxCustomers: 65,
+    avgCookiesPerSale: 6.3
+  },
+  {
+    location: "Tokyo",
+    minCustomers: 3,
+    maxCustomers: 24,
+    avgCookiesPerSale: 1.2
+  },
+  {
+    location: "Dubai",
+    minCustomers: 11,
+    maxCustomers: 38,
+    avgCookiesPerSale: 3.7
+  },
+  {
+    location: "Paris",
+    minCustomers: 20,
+    maxCustomers: 38,
+    avgCookiesPerSale: 2.3
+  },
+  {
+    location: "Lima",
+    minCustomers: 2,
+    maxCustomers: 16,
+    avgCookiesPerSale: 4.6
+  }
+];
+
+restaraunts.addRestaraunt = function() {
+
+}
+
+restaraunts.removeRestaraunt = function() {
+
+}
